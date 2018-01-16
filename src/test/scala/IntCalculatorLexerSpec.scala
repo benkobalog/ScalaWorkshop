a3ae9c31431443c82a4e6ea85abab50a7ded9626
benkobalog <benko.balog@gmail.com>
2018-01-16 10:54:46 +0100

    Make tests more readable

--- src/test/scala/IntCalculatorLexerSpec.scala
+++ src/test/scala/IntCalculatorLexerSpec.scala
@@ -4,33 +4,31 @@ import calclexer.IntCalculatorLexer.{Integer, Operator}
 
 class IntCalculatorLexerSpec extends FlatSpec with Matchers {
 
-  val result =
-    Vector(Integer(1), Operator('+'), Integer(2))
+  private val result = Vector(Integer(1), Operator('+'), Integer(2))
 
-  val result3 = result ++ Vector(Operator('*'),
-                                 Integer(9),
-                                 Operator('-'),
-                                 Integer(2))
+  private val result2 = result ++ Vector(Operator('*'),
+                                         Integer(9),
+                                         Operator('-'),
+                                         Integer(2))
 
   "SimpleTokenizer" should "process operators and single digit numbers" in {
     IntCalculatorLexer.run("1+2") should be(result)
-    IntCalculatorLexer.run("1+2*9-2") should be(result3)
+    IntCalculatorLexer.run("1+2*9-2") should be(result2)
   }
 
   it should "handle single whitespaces" in {
     IntCalculatorLexer.run("1 +2 ") should be(result)
-    IntCalculatorLexer.run(" 1 + 2 * 9 - 2 ") should be(result3)
+    IntCalculatorLexer.run(" 1 + 2 * 9 - 2 ") should be(result2)
   }
 
   it should "handle any number of whitespaces" in {
     IntCalculatorLexer.run("1    +2 ") should be(result)
-    IntCalculatorLexer.run(" 1 + 2 * 9    - \n 2  ") should be(result3)
+    IntCalculatorLexer.run(" 1 + 2 * 9    - \n 2  ") should be(result2)
   }
 
   it should "process integers with more than one digits" in {
-    val result2 =
-      Vector(Integer(123), Operator('+'), Integer(256))
-    IntCalculatorLexer.run("123 + 256") should be(result2)
+    IntCalculatorLexer.run("123 + 256") should be(
+      Vector(Integer(123), Operator('+'), Integer(256)))
   }
 
 }
